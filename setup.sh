912b6f2c82661865befce7809f25d9738c4d7be8
FaceLessOfficialNeq <151958189+FaceLessOfficialNew@users.noreply.github.com>
2023-12-07 01:11:45 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,22 @@
+
+echo """EM BOMBER INSTALL WAIT"""
+
+
+echo """Requirments Install"""
+
+echo -EMBOMBER LOADING '>>>                       [20%]\r'
+# some task
+sleep 2
+echo -EMBOMBER LOADING '>>>>>>>                   [40%]\r'
+# some task
+sleep 2
+echo -EMBOMBER LOADING '>>>>>>>>>>>>>>            [60%]\r'
+# some task
+sleep 2
+echo -EMBOMBER LOADING '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
+# some task
+sleep 2
+echo -EMBOMBER LOADING '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -Seccesfuly Load  '\n'
+
+echo """𝙏𝙃𝘼𝙉𝙆𝙎 𝙁𝙊𝙍 𝙐𝙎𝙄𝙉𝙂 𝙈𝙔 𝙏𝙊𝙊𝙇❤️-𝗡𝗼𝘄 𝘁𝘆𝗽𝗲 𝗲𝗺-𝗯𝗼𝗺𝗯𝗲𝗿.𝗽𝘆"""
